4a11c9e81d086d7047af06c4822d628e0a98b784
matteo.rzl <matteo.arzel@limayrac.fr>
2023-05-05 16:54:23 +0200

    build: fixes carousel

--- index.php
+++ index.php
@@ -2,31 +2,21 @@
 <html lang="fr" dir="ltr">
     <head>
         <?php include('header.html') ?>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Site E-commerce</title>
         <link rel="stylesheet" href="index.css">
         <link rel="stylesheet" href="index.js">
-        <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
     </head>
     <body>
         <article>
-            <div class="carousel" data-flickity='{"wrapAround": true, "autoPlay": true, "imagesLoaded":true, "freeScroll":true}'>
-                <div class="carousel-cell">
-                    <img class="w3-image" src="https://smash-images.photobox.com/original/5f04c1b41fd48d1b10ff27dfc90548bf13608845_Large-Print-lifestyle-3_1-2600.jpg">
-                </div>
-                <div class="carousel-cell">
-                    <img class="w3-image" src="https://smash-images.photobox.com/original/bca8e5fa7862a2cfaefc300c5b572e7a6dc6f3f3_Standard-Prints-lifestyle-3_1-2600.jpg">
-                </div>
-                <div class="carousel-cell">
-                    <img class="w3-image" src="https://smash-images.photobox.com/original/a422aed1a721e933961b19ea9e47e07fc71e0699_Acrylic-Prints-lifestyle-3_1-2600.jpg">
-                </div>
-            </div>
+            <?php require "carrousel.php" ?>
         </article>
-        <article>
-            <a href="https://mjfilrouge.azurewebsites.net/produit.php">produits</a>
-        </article>
+        <box>
+        <form action="produit.php">
+            <input type="submit" value="Produit" />
+        </form>
+        </box>
     </body>
 </html>

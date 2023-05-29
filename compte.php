3430d02c72c98de95ecb29aa83e1381e9d4e8c43
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-05-29 11:36:55 +0200

    build: Ajout squelette page compte et panier et lien avec les autres pages

---
+++ compte.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head> 
+        <?php include('header.html') ?>
+        <title>Compte</title>
+        <link rel="stylesheet" href="compte.css">
+    </head>
+    <body>
+        <article>
+            <div class="info_perso">
+                <div class="nom">Nom : </div>
+                <div class="prenom">Prenom : </div>
+                <div class="mail">Adresse mail : </div>
+                <div class="mdp">Mot de passe : </div>
+            </div>
+            <div class="adresses">
+                <div class="facturation">Adresses de facturation : <div>
+                <div class="livraison">Adresses de livraison : </div>
+            </div>
+            <div class="methode_paiement">
+                <div class="cartes">Cartes enregistées : </div>
+            </div>
+        </article>
+    </body>
+</html>
---
+++ panier.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head> 
+        <?php include('header.html') ?>
+        <title>Panier</title>
+    </head>
+    <body>
+    </body>
+</html>

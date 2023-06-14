a5d2a35b09b0d9a3146187e188613a52f765f3cb
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-06-14 17:55:22 +0200

    build: fixes test

--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "CloudSAdeaa70e5", "pwd" => "xewpom-hocmuk-5deWha", "Database" => "filrouge", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:filrougematteojulien.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+    $connectionInfo = array("UID" => "CloudSAdeaa70e5", "pwd" => "xewpom-hocmuk-5deWha", "Database" => "filrouge", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:filrougematteojulien.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+      }
+      echo "Connected successfully";
 ?>

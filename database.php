49c10704fb7ddf6252d98fe95f7b8116986fc0fe
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-06-14 17:58:05 +0200

    build: fixes database.php

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:filrougematteojulien.database.windows.net,1433; Database = filrouge", "CloudSAdeaa70e5", "xewpom-hocmuk-5deWha");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+// SQL Server Extension Sample Code:
+    $connectionInfo = array("UID" => "CloudSAdeaa70e5", "pwd" => "xewpom-hocmuk-5deWha", "Database" => "filrouge", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:filrougematteojulien.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+      }
+      echo "Connected successfully";
+?>

90cf743a7488687e5d06ba286e8c884f55209a8a
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-06-14 16:15:12 +0200

    build: fixes database.php

--- database.php
+++ database.php
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "CloudSAdeaa70e5", "pwd" => "{your_password_here}", "Database" => "filrouge", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "CloudSAdeaa70e5", "pwd" => "xewpom-hocmuk-5deWha", "Database" => "filrouge", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:filrougematteojulien.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>

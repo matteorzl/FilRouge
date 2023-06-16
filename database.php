e0ef549a97a0a613467078805b36b3804c0b7821
matteorzl <matteo.arzel@limayrac.fr>
2023-06-16 13:48:19 +0200

    build: fixes "database.php"

--- database.php
+++ database.php
@@ -7,7 +7,6 @@ $password = "xewpom-hocmuk-5deWha";
 try {
     $conn = new PDO("sqlsrv:server=$serverName;database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie !";
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }

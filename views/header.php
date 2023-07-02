d43b7e1b2a0828914dac49b4f3d451cbfdb6fa30
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-02 18:32:05 +0200

    build: fixes header

--- views/header.php
+++ views/header.php
@@ -121,12 +121,13 @@
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <?php if(!isset($_SERVER['PHP_SELF']) == "view/index.php"):?>
+          <?php var_dump( $_SERVER['PHP_SELF']) ?>
+          <?php if(!isset($_SERVER['PHP_SELF']) == "/views/index.php"):?>
           <li><a href="index.php" class="nav-link px-2 text-dark">Accueil</a></li>
           <?php else:?>
           <li><a href="index.php" class="nav-link px-2 text-light">Accueil</a></li>
           <?php endif;?>
-          
+
           <li><a href="produit.php" class="nav-link px-2 text-light">Produits</a></li>
           <li><a href="contact.php" class="nav-link px-2 text-light">Nous contactez</a></li>
           <li><a href="panier.php" class="nav-link px-2 text-light">Panier</a></li>

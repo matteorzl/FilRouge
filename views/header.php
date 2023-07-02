3cc8b1cd0bb315b911ccfb21cf2c13e23d1e58fd
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-02 17:14:32 +0200

    build fixes header

--- views/header.php
+++ views/header.php
@@ -121,11 +121,10 @@
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+          <li><a href="index.php" class="nav-link px-2 text-secondary">Acceuil</a></li>
+          <li><a href="produit.php" class="nav-link px-2 text-white">Produits</a></li>
+          <li><a href="contact.php" class="nav-link px-2 text-white">Nous contactez</a></li>
+          <li><a href="panier.php" class="nav-link px-2 text-white">Panier</a></li>
         </ul>
 
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">

3c5f108257c63bac408379064a1c3102bf0e9c6c
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-02 11:41:08 +0200

    build: fixes footer

--- views/footer.php
+++ views/footer.php
@@ -86,6 +86,25 @@
       .bd-mode-toggle {
         z-index: 1500;
       }
+      
+      .container{
+      	border-top: 0.5px solid lightgray;
+        margin-left:15%;
+        margin-right:15%;
+        }
+      .footer{
+      margin:5px;
+       display: flex;
+       flex-direction:row;
+      }
+      .reseaux{
+       list-style-type: none;
+       margin:5px;
+       display: flex;
+       justify-content: flex-end;
+       flex: 3;
+      }
+      .ms-3{margin:2px;}
     </style>
 
     
@@ -122,15 +141,15 @@
   </symbol>
 </svg>
 <div class="container">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+  <footer class="footer">
+    <div class="text_alter">
+      <a href="/" class="ref">
         <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
       </a>
       <span class="mb-3 mb-md-0 text-body-secondary">ProjetFilRouge_InstitutLimayrac &copy; 2023</span>
     </div>
 
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+    <ul class="reseaux">
       <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
       <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
       <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>

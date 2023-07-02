784326a308dd950c1e46d3ef7011c8f9eea27200
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-02 19:16:14 +0200

    build fixes logo on header

--- views/header.php
+++ views/header.php
@@ -116,8 +116,8 @@
   <div class="header">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <img src="/images/logo/logo.png"/>
+        <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img src="/images/logo/logo.png" width="30px" height="10px">
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">

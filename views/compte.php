1421983043c5c2b494d7d5106af5c90636281e6e
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-02 10:43:24 +0200

    build fixes header and compte

--- views/compte.php
+++ views/compte.php
@@ -1,6 +1,10 @@
 <?php
     session_start();
     require_once "database.php";
+    if(!isset($_SESSION["users"])){
+        header(Location: "login.php");
+        exit();
+    }
     require_once "header.php";
 ?>
 

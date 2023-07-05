37ac6159fe8635a5dcc02f199bb390d222e8926d
matteorzl <matteo.arzel@limayrac.fr>
2023-07-05 18:05:38 +0200

    build: fixes carousel size

--- views/index.php
+++ views/index.php
@@ -15,11 +15,25 @@ require_once "database.php";
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Site E-commerce</title>
         <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/carousel.css">
         <link rel="stylesheet" href="js/index.js">
+        <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
     </head>
     <body>
         <article>
-            <?php require "carousel.php" ?>
+            <div class="carousel" data-flickity='{"wrapAround": true, "autoPlay": 5000, "imagesLoaded":true, "freeScroll":true}'>
+                <div class="carousel-cell">
+                    <img class="w3-image" src="https://smash-images.photobox.com/original/5f04c1b41fd48d1b10ff27dfc90548bf13608845_Large-Print-lifestyle-3_1-2600.jpg">
+                </div>
+                <div class="carousel-cell">
+                    <img class="w3-image" src="https://smash-images.photobox.com/original/bca8e5fa7862a2cfaefc300c5b572e7a6dc6f3f3_Standard-Prints-lifestyle-3_1-2600.jpg">
+                </div>
+                <div class="carousel-cell">
+                    <img class="w3-image" src="https://smash-images.photobox.com/original/a422aed1a721e933961b19ea9e47e07fc71e0699_Acrylic-Prints-lifestyle-3_1-2600.jpg">
+                </div>
+            </div>
         </article>
         <div class="text-index">
             <h2>VENANT DES HAUTES TERRES D'ÉCOSSE NOS MEUBLES SONT IMMORTELS<h2>

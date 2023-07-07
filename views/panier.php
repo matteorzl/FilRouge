4810a61d36842c762621b99ccdc349a73f1d2e49
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-07 14:02:09 +0200

    build: fixes panier.php

--- views/panier.php
+++ views/panier.php
@@ -43,11 +43,11 @@
                         <td class="empty">Votre panier est vide</td>
                     </tr>
                     <tr>
-                        <td><img src="<?php echo $product['bin']; ?>" width="40px"></td>
-                        <td><?=$product["name"]?></td>
-                        <td><?=$product["price"]?>€</td>
-                        <td><?=$_SESSION["cart"][$product["product_id"]]?></td>
-                        <td><a href="panier.php?del=<?php echo $product["product_id"]?>"><img src="images/delete/delete.png" width="40px" padding="8px 0"></a></td>
+                        <td><img src="<?php echo $products['bin']; ?>" width="40px"></td>
+                        <td><?=$products["name"]?></td>
+                        <td><?=$products["price"]?>€</td>
+                        <td><?=$_SESSION["cart"][$products["product_id"]]?></td>
+                        <td><a href="panier.php?del=<?php echo $products["product_id"]?>"><img src="images/delete/delete.png" width="40px" padding="8px 0"></a></td>
                     </tr>
                     <tr>
                         <th>Total:€</th>

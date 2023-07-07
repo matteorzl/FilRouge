5d4182e9ad2c0ded9a580fc63d1742e83dcdc3f9
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-07 11:45:30 +0200

    build: fixes panier

--- views/panier.php
+++ views/panier.php
@@ -3,10 +3,9 @@
     require_once "database.php";
     require_once "header.php";
     $ids = array_keys($_SESSION["cart"]);
-    $stmt = $conn->query("SELECT p.*, i.bin FROM products p
-                          INNER JOIN images i ON p.image_id = i.image_id
-                          WHERE p.product_id IN ($ids)");  
+    $stmt = $conn->query("SELECT * FROM products WHERE product_id IN ($ids)");  
     $product = $stmt->fetch();
+    var_dump($product);
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -28,11 +27,11 @@
                         <td class="empty">Votre panier est vide</td>
                     </tr>
                     <tr>
-                        <td><img src="<?php echo $product['bin']; ?>" width="40px"></td>
+                        <td><img src="" width="40px"></td>
                         <td><?=$product["name"]?></td>
                         <td><?=$product["price"]?>€</td>
                         <td><?=$_SESSION["cart"][$product["product_id"]]?></td>
-                        <td><a href="panier.php?del=<?php echo $product["product_id"]?>"><img src="images/delete/delete.png" width="40px" padding="8px 0"></a></td>
+                        <td><a href=""><img src="images/delete/delete.png" width="40px" padding="8px 0"></a></td>
                     </tr>
                     <tr>
                         <th>Total:€</th>

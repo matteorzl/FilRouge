38866dc3e60d9671b1b1a61d36caf8bc4e0ce171
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-07 10:57:10 +0200

    build: fixes panier.php

--- views/panier.php
+++ views/panier.php
@@ -2,7 +2,16 @@
     session_start();
     require_once "database.php";
     require_once "header.php";
+
+    $total = 0;
+
     var_dump($_SESSION["cart"]);
+    
+    $id = $_SESSION["cart"];
+    $stmt = $conn->query("SELECT p.*, i.bin FROM products p
+                            INNER JOIN images i ON p.image_id = i.image_id
+                            WHERE p.product_id = $id");
+    
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -20,9 +29,21 @@
                         <th>Quantité</th>
                         <th>Supprimer</th>
                     </tr>
+                    <?php if(empty($id)):?>
                     <tr><td class="empty">Votre panier est vide</td></tr>
+                    <?php else {
+                                foreach ($stmt as $product):
+                                    $total += $product["price"] * $_SESSION["cart"][$product["product_id"]];
+                        ?>
                     <tr>
-                        <th>Total:€</th>
+                        <td><img src="<?php echo $product['bin']; ?>" width="40px"></td>
+                        <td><?=$product["name"]?></td>
+                        <td><?=$product["price"]?>€</td>
+                        <td><?=$_SESSION["cart"][$product["product_id"]]?></td>
+                        <td><a href="panier.php?del=<?php echo $product["product_id"]?>"><img src="images/delete/delete.png" width="40px" padding="8px 0"></a></td>
+                    </tr>
+                    <tr>
+                        <th>Total: <?php echo $total?>€</th>
                     </tr>
                 </table>
             </section>

118642238ae62bf5fed5c868cb78afb06afb0ba6
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-07 11:30:14 +0200

    build: fixes panier.php

--- views/panier.php
+++ views/panier.php
@@ -2,11 +2,11 @@
     session_start();
     require_once "database.php";
     require_once "header.php";
-    var_dump($_SESSION["cart"]);
     $ids = array_keys($_SESSION["cart"]);
     $stmt = $conn->query("SELECT p.*, i.bin FROM products p
                           INNER JOIN images i ON p.image_id = i.image_id
                           WHERE p.product_id = $ids");  
+    $product = $stmt->fetch();
     if (!$stmt) {
         echo "Erreur de requête : " . $conn->error;
     }
@@ -29,6 +29,13 @@
                     </tr>
                     <tr><td class="empty">Votre panier est vide</td></tr>
                     <tr>
+                    <tr>
+                        <td><img src="<?php echo $product['bin']; ?>" width="40px"></td>
+                        <td><?=$product["name"]?></td>
+                        <td><?=$product["price"]?>€</td>
+                        <td><?=$_SESSION["cart"][$product["product_id"]]?></td>
+                        <td><a href="panier.php?del=<?php echo $product["product_id"]?>"><img src="images/delete/delete.png" width="40px" padding="8px 0"></a></td>
+                    </tr>
                         <th>Total:€</th>
                     </tr>
                 </table>

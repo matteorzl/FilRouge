74770ae4a3c9a2e85875e81c6f316d10d5df68ef
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-08 16:32:34 +0200

    build: fixes delete user

--- views/backoffice/delete/deleteuser.php
+++ views/backoffice/delete/deleteuser.php
@@ -2,7 +2,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    require_once ".../database.php";
+    require_once "views\database.php";
     $id = $_GET["id"];
 
     $conn->query("DELETE FROM users WHERE user_id = $id");

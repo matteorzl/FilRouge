fc3f199d6a895cf92ab717bff1e5da8cd7d6452f
matteorzl <matteo.arzel@limayrac.fr>
2023-07-10 11:29:44 +0200

    build: fixes "Créer produit"

--- views/backoffice/products.php
+++ views/backoffice/products.php
@@ -28,7 +28,7 @@ if (isset($_GET["del"])) {
     <body>
         <div class="title-button">
             <h2>Produits</h2>
-            <button class="createproduct" type="button" href="create/createproduct.php">Créer Produit</button>
+            <a class="createproduct" href="create/createproduct.php">Créer Produit</a>
         </div>
         <div class="table-responsive small">
             <table class="table table-striped table-sm">

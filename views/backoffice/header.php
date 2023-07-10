429e2b6d3d3ab67f9e6c971d412ee73c30aa8079
matteorzl <matteo.arzel@limayrac.fr>
2023-07-10 10:50:30 +0200

    build: fixes header

--- views/backoffice/header.php
+++ views/backoffice/header.php
@@ -127,25 +127,25 @@
                   <li><a href="dashboard.php" class="nav-link px-2 text-light">Accueil</a></li>
                 <?php endif;?>
                 
-                <?php if(($_SERVER['PHP_SELF']) == "/views/category.php"):?>
+                <?php if(($_SERVER['PHP_SELF']) == "/views/backoffice/category.php"):?>
                   <li><a href="category.php" class="nav-link px-2 text-dark">Catégorie</a></li>
                 <?php else:?>
                   <li><a href="category.php" class="nav-link px-2 text-light">Catégorie</a></li>
                 <?php endif;?>
                 
-                <?php if(($_SERVER['PHP_SELF']) == "/views/products.php"):?>
+                <?php if(($_SERVER['PHP_SELF']) == "/views/backoffice/products.php"):?>
                   <li><a href="products.php" class="nav-link px-2 text-dark">Produits</a></li>
                 <?php else:?>
                   <li><a href="products.php" class="nav-link px-2 text-light">Produits</a></li>
                 <?php endif;?>
 
-                <?php if(($_SERVER['PHP_SELF']) == "/views/carousel.php"):?>
+                <?php if(($_SERVER['PHP_SELF']) == "/views/backoffice/carousel.php"):?>
                   <li><a href="carousel.php" class="nav-link px-2 text-dark">Carrousel</a></li>
                 <?php else:?>
                   <li><a href="carousel.php" class="nav-link px-2 text-light">Carrousel</a></li>
                 <?php endif;?>
 
-                <?php if(($_SERVER['PHP_SELF']) == "/views/users.php"):?>
+                <?php if(($_SERVER['PHP_SELF']) == "/views/backoffice/users.php"):?>
                   <li><a href="users.php" class="nav-link px-2 text-dark">Utilisateur</a></li>
                 <?php else:?>
                   <li><a href="users.php" class="nav-link px-2 text-light">Utilisateur</a></li>

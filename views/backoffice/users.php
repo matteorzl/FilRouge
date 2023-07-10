33530a7f29c530fe07954e34a0497efe1576a935
matteorzl <matteo.arzel@limayrac.fr>
2023-07-10 15:28:27 +0200

    build: fixes css

--- views/backoffice/users.php
+++ views/backoffice/users.php
@@ -27,7 +27,13 @@ if (isset($_GET["del"])) {
     </head>
     <body>
     <h2>Utilisateur</h2>
-      <div class="table-responsive small">
+        <div class="createuserbox">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+            </svg>
+            <a href="createuser.php" class="createuser">Créer Utilisateur</a>
+        </div>
+        <div class="table-responsive small">
         <table class="table table-striped table-sm">
             <tr>
                 <th>Nom</th>

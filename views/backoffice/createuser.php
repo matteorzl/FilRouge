82166cb4ce1acb455f80c64514a0d91b85a1ea05
matteorzl <matteo.arzel@limayrac.fr>
2023-07-11 11:33:25 +0200

    build: add createuser.php content

--- views/backoffice/createuser.php
+++ views/backoffice/createuser.php
@@ -5,9 +5,66 @@ if($_SESSION["users"]["role"] != 1 || !isset($_SESSION["users"])){
     exit();
   }
 
-require_once "header.php";
-require_once "../database.php";
+  require_once "../database.php";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if(isset($_POST["lastname"], $_POST["firstname"], $_POST["mail"], $_POST["pwd"], $_POST["role"])
+     && !empty($_POST["lastname"])&& !empty($_POST["firstname"])&& !empty($_POST["mail"])&& !empty($_POST["pwd"])&& !empty($_POST["role"])){
+    
+      $lastname = $_POST["lastname"];
+      $firstname = $_POST["firstname"];
+      $mail = $_POST["mail"];
+      $pwd = $_POST["pwd"];
+      $role = $_POST["role"];
+
+      //On verifie si un compte existe deja 
+      $sql = "SELECT * FROM users WHERE mail = ?";
+      $params = array($mail);
+      $stmt = $conn->prepare($sql);
+      $stmt->execute($params);
+      
+      $count = $stmt->fetchColumn();
+      
+      if ($count > 0) {
+      // L'adresse e-mail existe déjà
+      $_SESSION['error_message'] = "Cette adresse mail est déjà utilisée";
+      header('Location: login.php');
+      }else{
+
+
+      // Hash du mot de passe
+      $hashedpwd = password_hash($pwd, PASSWORD_BCRYPT);
+
+      $sql = "INSERT INTO users (lastname, firstname, mail, pwd, role) VALUES (?, ?, ?, ?, ?)";
+      $params = array($lastname, $firstname, $mail, $hashedpwd);
+
+      try {
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($params);
+
+        $user_id=$conn->lastinsertId();
+
+        $_SESSION['users'] = [
+          "user_id"=> $user_id,
+          "lastname" => $lastname,
+          "firstname" => $firstname,
+          "mail" => $mail,
+          "role" => $role,
+        ]; // Stocke les informations de l'utilisateur en session
+        $_SESSION['message'] = "L'incription a été validé";
+        header('Location: index.php');
+        exit();
+    } catch (PDOException $e) {
+        die("Erreur lors de l'inscription : " . $e->getMessage());
+    }
+  }
+}
+else{
+      echo "<script>alert(\"Toutes les informations de sont pas remplis\")</script>";
+}}
+
+  require_once "header.php";
 ?>
 
 <!DOCTYPE html>
@@ -35,8 +92,8 @@ require_once "../database.php";
 
                 <label for="role">Rôle</label>
                     <select name="role" id="role">
-                        <option value="">Utilisateur</option>
-                        <option value="">Administrateur</option>
+                        <option value="0">Utilisateur</option>
+                        <option value="1">Administrateur</option>
                     </select>
                 </label>
                 <input type="submit" class="createbutton" value="Créer utilisateur">

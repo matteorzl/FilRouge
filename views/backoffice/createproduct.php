a5eaa7bfe73a1af91927d2f00a50ecda34f49fa6
matteorzl <matteo.arzel@limayrac.fr>
2023-07-11 17:56:06 +0200

    build: fixes createproduct.php

--- views/backoffice/createproduct.php
+++ views/backoffice/createproduct.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $targetFile = $targetDir . basename($_FILES["image"]["name"]);
     move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile);
 
-    header('Location: products.php');
+    header('Location: ../products.php');
     exit();
 }
 

aa0909094e853aff44584fbeea2c7356fb1a7336
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-13 15:44:40 +0200

    build:  add create material and fixes products

--- views/backoffice/creatematerial.php
+++ views/backoffice/creatematerial.php
@@ -11,59 +11,26 @@ $stmt = null;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Vérifier que les données sont envoyées
-    if (!empty($_FILES['image']['name']) && isset($_POST['name']) && $_POST['name'] != "") {
+    if (isset($_POST['name']) && $_POST['name'] != "") {
         // Récupérer les valeurs du formulaire
         $name = $_POST["name"];
-        $location = "https://mjfilrouge.azurewebsites.net/views/images/category/";
-        $image = $location . basename($_FILES["image"]["name"]);
 
-        if (!extension_loaded('gd') || !function_exists('gd_info')) {
-            die('L\'extension GD n\'est pas activée. Veuillez vérifier la configuration de votre serveur.');
-        } else {
-            // Déplacer l'image téléchargée vers le dossier approprié
-            $targetDir = "../images/category/";
-            // Créer le dossier s'il n'existe pas
-            if (!is_dir($targetDir)) {
-                mkdir($targetDir, 0755, true);
-            }   
-            $targetFile = $targetDir . basename($_FILES["image"]["name"]);
-            
-            $uploadOk = 1;
-            $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-        }
+        // Insérer le nom dans la base de données
+        $sql = "INSERT INTO materials (name) VALUES (?)";
+        $params = array($name);
 
-        // Vérifier si le fichier image est réel ou une fausse image
-        $check = getimagesize($_FILES["image"]["tmp_name"]);
-        if ($check !== false) {
-            // Autoriser certains formats d'image (vous pouvez ajouter d'autres formats si nécessaire)
-            $allowedFormats = array("jpg", "jpeg", "png", "gif");
-            if (in_array($imageFileType, $allowedFormats)) {
-                if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-                    // Insérer le nom et l'image dans la base de données
-                    $sql = "INSERT INTO categories (name, image1) VALUES (?, ?)";
-                    $params = array($name, $image);
-
-                    try {
-                        $stmt = $conn->prepare($sql);
-                        $stmt->execute($params);
-                        header('Location: category.php');
-                        exit();
-                    } catch (PDOException $e) {
-                        // Afficher l'erreur SQL
-                        echo "Erreur SQL : " . $e->getMessage() . "<br>";
-                        echo "Code d'erreur SQL : " . $e->getCode() . "<br>";
-                        echo "Informations complémentaires : ";
-                        print_r($stmt->errorInfo());
-                        die();
-                    }
-                } else {
-                    $message = "Erreur lors du téléchargement de l'image.";
-                }
-            } else {
-                $message = "Formats d'image autorisés : JPG, JPEG, PNG, GIF uniquement.";
-            }
-        } else {
-            $message = "Le fichier sélectionné n'est pas une image valide.";
+        try {
+            $stmt = $conn->prepare($sql);
+            $stmt->execute($params);
+            header('Location: material.php');
+            exit();
+        } catch (PDOException $e) {
+            // Afficher l'erreur SQL
+            echo "Erreur SQL : " . $e->getMessage() . "<br>";
+            echo "Code d'erreur SQL : " . $e->getCode() . "<br>";
+            echo "Informations complémentaires : ";
+            print_r($stmt->errorInfo());
+            die();
         }
     } else {
         $message = "Veuillez remplir tous les champs.";
@@ -77,19 +44,16 @@ require_once "header.php";
 <html lang="fr" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="css/createcategory.css">
+        <link rel="stylesheet" href="css/creatematerial.css">
     </head>
     <body>
-        <div class="createcategory">
-            <h1 class="titleCreateCategory">Créer Categorie</h1>
-            <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>" enctype="multipart/form-data">
+        <div class="creatematerial">
+            <h1 class="titleCreateMaterial">Créer Matériau</h1>
+            <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
                 <label for="name">Nom</label>
                 <input type="text" id="name" name="name" required>
 
-                <label for="image">Image</label>
-                <input type="file" id="image" name="image" required>
-
-                <input type="submit" class="createbutton" value="Créer Catégorie">
+                <input type="submit" class="createbutton" value="Créer Matériau">
             </form>
        </div>
     </body>

564de2af5812e0efc15e59b0ddcfb707f3fbb853
matteorzl <matteo.arzel@limayrac.fr>
2023-07-14 12:16:09 +0200

    build: test carousel update

--- views/backoffice/carousel.php
+++ views/backoffice/carousel.php
@@ -1,41 +1,22 @@
 <?php
-session_start();
-if($_SESSION["users"]["role"] != 1 || !isset($_SESSION["users"])){
-    header('Location: ../login.php');
-    exit();
-  }
+// Récupérer les informations du carrousel à partir de la base de données
+$stmt = $conn->query("SELECT * FROM carousel");
+$carousel = $stmt->fetch(PDO::FETCH_ASSOC);
 
-require_once "header.php";
-require_once "../database.php";
+// Afficher les images du carrousel avec un bouton "Changer"
 ?>
-
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/carousel.css">
-        <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/dashboard/">
-        <link href="../boostrap/assets/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
-    </head>
-    <body>
+<div>
     <h2>Carrousel</h2>
-      <div class="table-responsive small">
-        <table class="table table-striped table-sm">
-        <?php
-            $stmt = $conn->query("SELECT image1,image2,image3 FROM carousel");
-
-            while (($row = $stmt->fetch())) {?>
-                <?=$row['image1']?>
-                <?=$row['image2']?>
-                <?=$row['image3']?>
-        <?php 
-            }
-        ?>
-        </table>
-    </body>
-    <footer>
-        <?php require "footer.php" ?>
-    </footer> 
-</html>
+    <div>
+        <img src="<?=$carousel['image1']?>" alt="Image 1">
+        <button class="changebutton" onclick="window.location.href='modifycarousel.php?carousel_id=<?=$carousel['carousel_id']?>&image_number=1'">Changer</button>
+    </div>
+    <div>
+        <img src="<?=$carousel['image2']?>" alt="Image 2">
+        <button class="changebutton" onclick="window.location.href='modifycarousel.php?carousel_id=<?=$carousel['carousel_id']?>&image_number=2'">Changer</button>
+    </div>
+    <div>
+        <img src="<?=$carousel['image3']?>" alt="Image 3">
+        <button class="changebutton" onclick="window.location.href='modifycarousel.php?carousel_id=<?=$carousel['carousel_id']?>&image_number=3'">Changer</button>
+    </div>
+</div>

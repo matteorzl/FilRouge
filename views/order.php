898dc6d41ff9f01accd1a19f12d2828ff029c7c4
matteorzl <matteo.arzel@limayrac.fr>
2023-07-14 15:56:04 +0200

    build: fixes order.php

--- views/order.php
+++ views/order.php
@@ -25,31 +25,31 @@
         <link href="boostrap/assets/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
-        <?php
-            foreach ($order as $row) {
-                $deliveryId = $row["delivery_id"];
-                $deliveryaddress = $conn->query("SELECT address_1 FROM deliveries WHERE delivery_id = $deliveryId");
-                ?>
-            <form method="post" action="orderproduct.php?id=<?=$row['order_id']?>" class="form_list_prod">
+    <?php
+        foreach ($order as $row) {
+            $deliveryId = $row["delivery_id"];
+            $deliveryaddress = $conn->query("SELECT address_1 FROM deliveries WHERE delivery_id = $deliveryId");
+            $deliveryRow = $deliveryaddress->fetch(PDO::FETCH_ASSOC);
+        ?>
+            <form method="post" action="orderproduct.php?id=<?= $row['order_id'] ?>" class="form_list_prod">
                 <button type="submit" class="button_liste">
                     <div class="produit_img">
-                        <h4>Commande numero: <?php $row["order_id"]?></h4>
+                        <h4>Commande numero: <?= $row["order_id"] ?></h4>
                     </div>
-                       <div class="info_produit">
-                        <p><?=$row['date_order']?></p>
-                        <p><?php echo $deliveryaddress; ?></p>
-                        <p><?=$row['date_delivery']?></p>
+                    <div class="info_produit">
+                        <p><?= $row['date_order'] ?></p>
+                        <p><?= $deliveryRow['address_1'] ?></p>
+                        <p><?= $row['date_delivery'] ?></p>
                     </div>
                     <div class="quantite_prix">
-                        <h4><?=$row['status']?>€</h4>
+                        <h4><?= $row['status'] ?>€</h4>
                         <p>Cliquer pour afficher les details</p>
                     </div>
                 </button>
             </form>
-    <?php 
+        <?php
         }
     ?>
-
     </body>
     <footer>
         <?php require "footer.php" ?>

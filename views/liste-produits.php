2503becab96f325f2e010d5d5de326bef2c5c31f
Julien Blanchon <julien.blanchon@limayrac.fr>
2023-07-20 16:26:33 +0200

    build: fixes liste produit

--- views/liste-produits.php
+++ views/liste-produits.php
@@ -105,7 +105,8 @@ $products = $stmtProducts->fetchAll(PDO::FETCH_ASSOC);
 
 <?php foreach ($products as $row) {
     $materialId = $row["material_id"];
-    $materialname = $conn->query("SELECT [name] FROM materials WHERE material_id = $materialId");
+    $stmt = $conn->query("SELECT [name] FROM materials WHERE material_id = $materialId");
+    $materialname = $stmt->fetchAll(PDO::FETCH_ASSOC);
     ?>
     <form method="post" action="produit.php?id=<?php echo $row['product_id']; ?>" class="form_list_prod">
         <button type="submit" class="button_liste">
